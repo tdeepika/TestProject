3863bfce733b6af366979086773f947bfe877faa
Deepika Thangavelu <Deepika.Thangavelu@hmhco.com>
2016-12-26 15:49:08 +0530

    comm

--- src/main/java/com/cable/controller/CableController.java
+++ src/main/java/com/cable/controller/CableController.java
@@ -1,19 +1,31 @@
 package com.cable.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-
-
-@Controller
 public class CableController {
 	
-	@RequestMapping("/home1ff")
-	public ModelAndView home() {
+	
+	public void hocvdme() {
 		System.out.println("Controller");
-		String message = "Hello World, Spring MVC";
-		return new ModelAndView("main", "message", message);
+		System.out.println("Controller");
+		String message = "Hello WorlfdgdfgdfgVC";
+		System.out.println("Controller");
+		String message1 = "Hello WorlfdgdfgdfgVC";
+		System.out.println("Controller");
+		String message2 = "Hello WorlfdgdfgdfgVC";
+		System.out.println("Controller");
+		String message3 = "Hello WorlfdgdfgdfgVC";
+		System.out.println("Controller");
+		String message4 = "Hello WordfsslfdgdfgdfgVC";
+		System.out.println("Controller");
+		String message5 = "Hello WorlfdgdfgdfgVC";
+		System.out.println("Controller");
+		String message6 = "Hello WorlfdgdfgdfgVC";
+		System.out.println("Controller");
+		String message7 = "Hello WorlfdgdfgdfgVC";
+		
+		System.out.println("Controller");
+		String message8 = "Hello WorlfvbcbdgdfgdfgVC";
+		String message0= "Hello WorlfdgdfgdfgVC";
+		String message9 = "Hello WorlfdgdfgdfgVC";
 	}
 
 	
